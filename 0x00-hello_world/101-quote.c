ccd588afee1d0b5c0ecf9aef5c5c68fd2ed044f6
nwigwe nelson <nelsonchinedu866@gmail.com>
2023-03-17 07:43:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-* Description: print a quote using write function
-* write(int fd,const void *buf,size_t count)
-*
-* Return: 1
-*/
+ * main - Entry point 
+ * Description: print a quote using write function
+ * write(int fd,const void *buf,size_t count)
+ *
+ * Return: 1
+ */
 
-Int main(void)
-{ char quo[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-Write(1, quo, 59);
-return (1);
+int main(void)
+
+{ char quo []="and that piece of art is useful\"-Dora Korpar,2015-10-19\n";
+	Write (1,quo,59);
+	Return (1)
 }
